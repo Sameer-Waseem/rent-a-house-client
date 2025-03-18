a0f316dcd3d415b514021ceda1e21fdd7f09c1a5
Sameer-Waseem <sameerwaseem969@gmail.com>
2025-03-18 23:39:13 +0500

    Setup apiClient using axios

--- src/components/HouseListing.tsx
+++ src/components/HouseListing.tsx
@@ -1,6 +1,15 @@
 import Button from "@mui/material/Button";
+import { useEffect } from "react";
+import axiosInstance from "../services/apiClient";
 
 const HouseListing = () => {
+  useEffect(() => {
+    axiosInstance
+      .get("/house")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return <Button variant={"contained"}>sam</Button>;
 };
 
