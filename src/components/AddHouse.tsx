95c9f81d8ed0e5eab67205e5294ca5f6081a04cd
Sameer-Waseem <sameerwaseem969@gmail.com>
2025-04-16 10:18:56 +0500

    Added yup validation and optimized code

--- src/components/AddHouse.tsx
+++ src/components/AddHouse.tsx
@@ -10,19 +10,21 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import AddIcon from "../assets/add.png";
 import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import * as Yup from "yup";
+
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
-import { Field, FieldProps, Form, Formik } from "formik";
+import { ErrorMessage, Field, FieldProps, Form, Formik } from "formik";
 
 interface FormValues {
-  area: number | null;
   type: string;
+  area: number | null;
   rent: number | null;
   highlights: string;
   description: string;
@@ -33,17 +35,31 @@ interface FormValues {
   electricity_supply: string;
 }
 
+interface FormFieldProps {
+  name: string;
+  label: string;
+  type: "text" | "number";
+  margin: string;
+  padding: string;
+}
+
+interface FormRadioProps {
+  name: string;
+  label: string;
+  children: ReactNode;
+}
+
 const AddHouse = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   const initialValues: FormValues = {
-    area: null,
     type: "appartement",
-    rent: null,
+    area: 0,
+    rent: 0,
     highlights: "",
     description: "",
-    rooms: null,
-    bathrooms: null,
+    rooms: 0,
+    bathrooms: 0,
     water_supply: "yes",
     gas_supply: "yes",
     electricity_supply: "yes",
@@ -85,210 +101,225 @@ const AddHouse = () => {
       >
         <DialogTitle
           id={"add-house-dialog-title"}
-          variant={"h6"}
+          variant={"h5"}
           sx={{ padding: "16px 24px" }}
         >
           Add a new house
         </DialogTitle>
 
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          <Form>
-            <DialogContent>
-              <DialogContentText id={"add-house-dialog-description"}>
-                Please provide the house details.
-              </DialogContentText>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validation}
+          onSubmit={handleSubmit}
+        >
+          {() => (
+            <Form>
+              <DialogContent>
+                <DialogContentText id={"add-house-dialog-description"}>
+                  Please provide the house details.
+                </DialogContentText>
 
-              <FormControl margin={"dense"}>
-                <FormLabel>Type</FormLabel>
-                <Field name={"type"}>
-                  {({ field }: FieldProps) => (
-                    <RadioGroup
-                      {...field}
-                      aria-labelledby={"house-type"}
-                      sx={{ flexDirection: "row" }}
-                    >
-                      <FormControlLabel
-                        value={"appartement"}
-                        control={<Radio />}
-                        label={"Appartement"}
-                      />
-                      <FormControlLabel
-                        value={"plot"}
-                        control={<Radio />}
-                        label={"Plot"}
-                      />
-                    </RadioGroup>
-                  )}
-                </Field>
-              </FormControl>
+                <FormRadio name={"type"} label={"Type"}>
+                  <FormControlLabel
+                    value={"appartement"}
+                    control={<Radio />}
+                    label={"Appartement"}
+                  />
+                  <FormControlLabel
+                    value={"plot"}
+                    control={<Radio />}
+                    label={"Plot"}
+                  />
+                </FormRadio>
 
-              <Box display={"flex"}>
-                <Field name={"area"}>
-                  {({ field }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      id={"area"}
-                      label={"Area"}
-                      fullWidth
-                      type={"number"}
-                      sx={{ margin: "4px 4px 4px 0" }}
+                <Box display={"flex"}>
+                  <FormField
+                    name={"area"}
+                    label={"Area (sq. mt)"}
+                    type={"number"}
+                    margin={"4px 4px 4px 0"}
+                    padding={"0 4px 0 0"}
+                  />
+
+                  <FormField
+                    name={"rent"}
+                    label={"Rent (PKR)"}
+                    type={"number"}
+                    margin={"4px 0 4px 4px"}
+                    padding={"0 4px"}
+                  />
+                </Box>
+
+                <Box display={"flex"}>
+                  <FormField
+                    name={"highlights"}
+                    label={"Highlights"}
+                    type={"text"}
+                    margin={"4px 0"}
+                    padding={"0 4px"}
+                  />
+                </Box>
+
+                <Box display={"flex"}>
+                  <FormField
+                    name={"description"}
+                    label={"Description"}
+                    type={"text"}
+                    margin={"4px 0"}
+                    padding={"0 4px"}
+                  />
+                </Box>
+
+                <Box display={"flex"}>
+                  <FormField
+                    name={"rooms"}
+                    label={"Rooms"}
+                    type={"number"}
+                    margin={"4px 4px 4px 0"}
+                    padding={"0 4px"}
+                  />
+
+                  <FormField
+                    name={"bathrooms"}
+                    label={"Bathrooms"}
+                    type={"number"}
+                    margin={"4px 0 4px 4px"}
+                    padding={"0 4px"}
+                  />
+                </Box>
+
+                <Box display={"flex"} justifyContent={"space-between"}>
+                  <FormRadio name={"water_supply"} label={"Water Supply"}>
+                    <FormControlLabel
+                      value={"yes"}
+                      control={<Radio />}
+                      label={"Yes"}
                     />
-                  )}
-                </Field>
-
-                <Field name={"rent"}>
-                  {({ field }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      id={"rent"}
-                      label={"Rent"}
-                      fullWidth
-                      type={"number"}
-                      sx={{ margin: "4px 0 4px 4px" }}
+                    <FormControlLabel
+                      value={"no"}
+                      control={<Radio />}
+                      label={"no"}
                     />
-                  )}
-                </Field>
-              </Box>
+                  </FormRadio>
 
-              <Box display={"flex"}>
-                <Field name={"highlights"}>
-                  {({ field }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      id={"highlights"}
-                      label={"Highlights"}
-                      fullWidth
-                      sx={{ margin: "4px 0" }}
+                  <FormRadio name={"gas_supply"} label={"Gas Supply"}>
+                    <FormControlLabel
+                      value={"yes"}
+                      control={<Radio />}
+                      label={"Yes"}
                     />
-                  )}
-                </Field>
-              </Box>
-
-              <Box display={"flex"}>
-                <Field name={"description"}>
-                  {({ field }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      id={"description"}
-                      label={"Description"}
-                      fullWidth
-                      sx={{ margin: "4px 0" }}
+                    <FormControlLabel
+                      value={"no"}
+                      control={<Radio />}
+                      label={"no"}
                     />
-                  )}
-                </Field>
-              </Box>
+                  </FormRadio>
 
-              <Box display={"flex"}>
-                <Field name={"rooms"}>
-                  {({ field }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      id={"rooms"}
-                      label={"Rooms"}
-                      fullWidth
-                      type={"number"}
-                      sx={{ margin: "4px 4px 4px 0" }}
+                  <FormRadio
+                    name={"electricity_supply"}
+                    label={"Electricity Supply"}
+                  >
+                    <FormControlLabel
+                      value={"yes"}
+                      control={<Radio />}
+                      label={"Yes"}
                     />
-                  )}
-                </Field>
-
-                <Field name={"bathrooms"}>
-                  {({ field }: FieldProps) => (
-                    <TextField
-                      {...field}
-                      id={"bathrooms"}
-                      label={"Bathrooms"}
-                      fullWidth
-                      type={"number"}
-                      sx={{ margin: "4px 0 4px 4px" }}
+                    <FormControlLabel
+                      value={"no"}
+                      control={<Radio />}
+                      label={"no"}
                     />
-                  )}
-                </Field>
-              </Box>
+                  </FormRadio>
+                </Box>
+              </DialogContent>
 
-              <Box display={"flex"} justifyContent={"space-between"}>
-                <FormControl margin={"dense"}>
-                  <FormLabel>Water Supply</FormLabel>
-                  <Field name={"water_supply"}>
-                    {({ field }: FieldProps) => (
-                      <RadioGroup {...field} aria-labelledby={"water-supply"}>
-                        <FormControlLabel
-                          value={"yes"}
-                          control={<Radio />}
-                          label={"Yes"}
-                        />
-                        <FormControlLabel
-                          value={"no"}
-                          control={<Radio />}
-                          label={"no"}
-                        />
-                      </RadioGroup>
-                    )}
-                  </Field>
-                </FormControl>
-
-                <FormControl margin={"dense"}>
-                  <FormLabel>Gas Supply</FormLabel>
-                  <Field name={"gas_supply"}>
-                    {({ field }: FieldProps) => (
-                      <RadioGroup {...field} aria-labelledby={"gas-supply"}>
-                        <FormControlLabel
-                          value={"yes"}
-                          control={<Radio />}
-                          label={"Yes"}
-                        />
-                        <FormControlLabel
-                          value={"no"}
-                          control={<Radio />}
-                          label={"no"}
-                        />
-                      </RadioGroup>
-                    )}
-                  </Field>
-                </FormControl>
-
-                <FormControl margin={"dense"}>
-                  <FormLabel>Electricity Supply</FormLabel>
-                  <Field name={"electricity_supply"}>
-                    {({ field }: FieldProps) => (
-                      <RadioGroup
-                        {...field}
-                        aria-labelledby={"electricity-supply"}
-                      >
-                        <FormControlLabel
-                          value={"yes"}
-                          control={<Radio />}
-                          label={"Yes"}
-                        />
-                        <FormControlLabel
-                          value={"no"}
-                          control={<Radio />}
-                          label={"no"}
-                        />
-                      </RadioGroup>
-                    )}
-                  </Field>
-                </FormControl>
-              </Box>
-            </DialogContent>
-
-            <DialogActions sx={{ padding: "0 28px 16px" }}>
-              <Button
-                onClick={handleClose}
-                variant={"contained"}
-                color={"error"}
-              >
-                Close
-              </Button>
-              <Button type={"submit"} variant={"contained"} color={"primary"}>
-                Add
-              </Button>
-            </DialogActions>
-          </Form>
+              <DialogActions sx={{ padding: "0 28px 16px" }}>
+                <Button
+                  onClick={handleClose}
+                  variant={"contained"}
+                  color={"error"}
+                >
+                  Close
+                </Button>
+                <Button type={"submit"} variant={"contained"} color={"primary"}>
+                  Add
+                </Button>
+              </DialogActions>
+            </Form>
+          )}
         </Formik>
       </Dialog>
     </>
   );
 };
+
+const FormField = ({ name, label, type, margin, padding }: FormFieldProps) => {
+  return (
+    <Box width={"100%"} display={"flex"} flexDirection={"column"}>
+      <Field name={name}>
+        {({ field }: FieldProps) => (
+          <TextField
+            {...field}
+            id={name}
+            label={label}
+            fullWidth
+            type={type}
+            sx={{ margin }}
+          />
+        )}
+      </Field>
+
+      <ErrorMessage
+        name={name}
+        render={(err) => (
+          <Typography variant={"caption"} color={"error"} padding={padding}>
+            {err}
+          </Typography>
+        )}
+      />
+    </Box>
+  );
+};
+
+const FormRadio = ({ name, label, children }: FormRadioProps) => {
+  return (
+    <FormControl margin={"dense"}>
+      <FormLabel>{label}</FormLabel>
+
+      <Field name={name}>
+        {({ field }: FieldProps) => (
+          <RadioGroup
+            {...field}
+            aria-labelledby={"house-type"}
+            sx={{ flexDirection: "row" }}
+          >
+            {children}
+          </RadioGroup>
+        )}
+      </Field>
+    </FormControl>
+  );
+};
+
+const validation = Yup.object().shape({
+  type: Yup.string().trim().default("appartement").required(),
+  area: Yup.number().min(1).max(99999).required("Area is required."),
+  rent: Yup.number().min(1).max(99999).required("Rent is required."),
+  highlights: Yup.string()
+    .trim()
+    .min(3)
+    .max(55)
+    .required("Highlights is required."),
+  description: Yup.string()
+    .trim()
+    .min(10)
+    .max(1055)
+    .required("Description is required."),
+  rooms: Yup.number().min(1).max(255).required("Rooms are required."),
+  bathrooms: Yup.number().min(1).max(255).required("Bathrooms are required."),
+  water_supply: Yup.string().default("yes").required(),
+  gas_supply: Yup.string().default("yes").required(),
+  electricity_supply: Yup.string().default("yes").required(),
+});
 
 export default AddHouse;
